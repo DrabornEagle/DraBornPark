e079f4cd1dd40c5001b8505f04f87017425771c5
DrabornEagle <draborneagle@gmail.com>
2026-08-27 09:21:50 +0300

    db: publish DraBornPark v1.0.18 release policy

---
+++ supabase/migrations/20260827061500_dkd_drabornpark_v118_release_policy.sql
@@ -0,0 +1,16 @@
+insert into public.dkd_drabornpark_app_config(dkd_key,dkd_value,dkd_updated_at,dkd_updated_by)
+values (
+  'update_policy',
+  jsonb_build_object(
+    'force_update_enabled', false,
+    'minimum_version_code', 17,
+    'latest_version', '1.0.18',
+    'latest_version_code', 18
+  ),
+  now(),
+  null
+)
+on conflict (dkd_key) do update set
+  dkd_value=excluded.dkd_value,
+  dkd_updated_at=excluded.dkd_updated_at,
+  dkd_updated_by=excluded.dkd_updated_by;
